b79d678db53770ad674ed71cd412af3dfa893008
Thisisme222 <dylanwizard22@gmail.com>
2019-03-02 12:02:00 +0800

    update

--- lesson 8/app.js
+++ lesson 8/app.js
@@ -54,8 +54,34 @@ baddie = game.add.sprite(760,20,'baddie');
 	baddie.body.bounce.y = 0.2;
 	baddie.body.gravity.y = 300;
 	baddie.body.collideWorldBounds = true;
+	stars = game.add.physicsGroup();
+	stars.enableBody = true;
+	for(var i =0; i<12; i++){
+		var stars.create(i *70,0,'stars');
+		star.body.gravity.y = 200;
+		star.body.bounce.y =0.7 + random() *0.2;
+	}
 }
 
 function update(){
+game.physics.arcade.collide(player,platforms);
+game.physics.arcade.collide(stars,platforms);
+game.physics.arcade.collide(enemy1,platforms);
 
+player.body.velocity.x = 0;
+
+if(cursors.left.isDown){
+	player.body.velocity.x = -150;
+	player.animations.play('left');
+} else if (cursors.right.isDown){
+player.body.velocity.x = 150;
+player.animations.play('right');
+}
+else {
+	player.animations.stop();
+	player.frame =4;
+}
+if(cursors.up.isDown && player.body.touching.down){
+	player.body.velocity.y=-300;
+}
 }
